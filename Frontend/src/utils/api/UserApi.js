15b93eed882609b19148a01db04dfb4876bb5fd1
prasangautam07 <gprasan729@gmail.com>
2025-07-12 16:22:42 +0545

    api

--- Frontend/src/utils/api/UserApi.js
+++ Frontend/src/utils/api/UserApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { toast } from 'react-toastify';
-const localHost=true;
+const localHost=false;
 const apiUrl = localHost ? 'http://localhost:3000/api' : 'https://assessment-management-system-3gj3.onrender.com/api';
 
 //const apiUrl = 'https://assessment-management-system-3gj3.onrender.com/api';
